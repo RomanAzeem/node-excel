3b1daa3af6d2ab603f13916444fc775b83757173
RomanAzeem <romanazeem007@gmail.com>
2022-06-23 15:53:00 +0500

    carrier obejct updated

--- static/carriers.js
+++ static/carriers.js
@@ -2,46 +2,46 @@ module.exports = {
     carriers: [
         {
             service: "ST",
-            carrierId: "PRMD",
-            additionalCarrierID: "PROMED",
-            fullName: "ProMed Delivery",
+            carrierId: "AEX",
+            additionalCarrierID: "AEX",
+            fullName: "American Eagle Express",
+            productTypes: "vape",
+            Rate: "20.00"
+
+        },
+        {
+            service: "ST",
+            carrierId: "CAPE",
+            additionalCarrierID: "CAPEX",
+            fullName: "Capital Express",
+            productTypes: "vape",
+            Rate: "20.00"
+
+        },
+        {
+            service: "ST",
+            carrierId: "CSL",
+            additionalCarrierID: "CS Logistics",
+            fullName: "CS Logistics",
+            productTypes: "vape",
+            Rate: "20.00"
+
+        },
+        {
+            service: "ST",
+            carrierId: "DI",
+            additionalCarrierID: "Deliver-It",
+            fullName: "DeliverIT",
             productTypes: "beer,spirit,wine,vape",
             Rate: "20.00"
 
         },
         {
             service: "ST",
-            carrierId: "ZIP",
-            additionalCarrierID: "ZIP",
-            fullName: "Zip Express",
-            productTypes: "beer,spirit,vape",
-            Rate: "20.00"
-
-        },
-        {
-            service: "ST",
-            carrierId: "MERC",
-            additionalCarrierID: "MERC",
-            fullName: "Mercury",
-            productTypes: "beer,spirit,wine,vape",
-            Rate: "20.00"
-
-        },
-        {
-            service: "ST",
-            carrierId: "STAT",
-            additionalCarrierID: "STAT",
-            fullName: "STAT Overnight",
-            productTypes: "beer,spirit,wine,vape",
-            Rate: "11.99"
-
-        },
-        {
-            service: "ST",
-            carrierId: "SONIC",
-            additionalCarrierID: "SONIC",
-            fullName: "Sonic Systems Inc",
-            productTypes: "beer,spirit,wine,vape",
+            carrierId: "GLS",
+            additionalCarrierID: "GLS",
+            fullName: "General Logistics Systems",
+            productTypes: "beer,spirit,wine",
             Rate: "20.00"
 
         },
@@ -65,40 +65,55 @@ module.exports = {
         },
         {
             service: "ST",
-            carrierId: "AEX",
-            additionalCarrierID: "AEX",
-            fullName: "American Eagle Express",
-            productTypes: "vape",
-            Rate: "20.00"
-
-        },
-        {
-            service: "ST",
-            carrierId: "GLS",
-            additionalCarrierID: "GLS",
-            fullName: "General Logistics Systems",
-            productTypes: "beer,spirit,wine",
-            Rate: "20.00"
-
-        },
-        {
-            service: "ST",
-            carrierId: "DI",
-            additionalCarrierID: "Deliver-It",
-            fullName: "DeliverIT",
+            carrierId: "MERC",
+            additionalCarrierID: "MERC",
+            fullName: "Mercury",
             productTypes: "beer,spirit,wine,vape",
             Rate: "20.00"
 
         },
         {
             service: "ST",
-            carrierId: "CSL",
-            additionalCarrierID: "CS Logistics",
-            fullName: "CS Logistics",
-            productTypes: "vape",
+            carrierId: "PRMD",
+            additionalCarrierID: "PROMED",
+            fullName: "ProMed Delivery",
+            productTypes: "beer,spirit,wine,vape",
             Rate: "20.00"
 
         },
+        {
+            service: "ST",
+            carrierId: "SONIC",
+            additionalCarrierID: "SONIC",
+            fullName: "Sonic Systems Inc",
+            productTypes: "beer,spirit,wine,vape",
+            Rate: "20.00"
 
+        },
+        {
+            service: "ST",
+            carrierId: "STAT",
+            additionalCarrierID: "STAT",
+            fullName: "STAT Overnight",
+            productTypes: "beer,spirit,wine,vape",
+            Rate: "11.99"
+
+        },
+        {
+            service: "ST",
+            carrierId: "UDS",
+            additionalCarrierID: "UDS",
+            fullName: "United Delivery Service",
+            productTypes: "vape",
+            Rate: "20.00"
+        },
+        {
+            service: "ST",
+            carrierId: "ZIP",
+            additionalCarrierID: "ZIP",
+            fullName: "Zip Express",
+            productTypes: "beer,spirit,vape",
+            Rate: "20.00"
+        }
     ]
 }
